179c7b01df27883f3eaa8152f486527437ec0221
wmartinezm <wlsn.martinez@gmail.com>
2023-09-06 16:09:48 -0600

    Update test.c file to check behavior on main loop and thread.

--- test/test.c
+++ test/test.c
@@ -1,28 +1,58 @@
 #include <stdint.h>
+#include <stdio.h>
 #include <unity.h>
+#include <zephyr.h>
+#include <device.h>
+#include <devicetree.h>
+#include <drivers/gpio.h>
+#include "LED_Ctrl.h"
+
+#define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+
+#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
+#define PIN0	DT_GPIO_PIN(LED0_NODE, gpios)
+#define FLAGS0	DT_GPIO_FLAGS(LED0_NODE, gpios)
+#define LED1	DT_GPIO_LABEL(LED1_NODE, gpios)
+#define PIN1	DT_GPIO_PIN(LED1_NODE, gpios)
+#define FLAGS1	DT_GPIO_FLAGS(LED1_NODE, gpios)
+
+
 
 void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_variable_assignment()
+void test_main_loop_enable(void)
 {
-    int x = 1;
-    TEST_ASSERT_TRUE_MESSAGE(x == 1,"Variable assignment failed.");
+	const struct device *dev = device_get_binding(LED0);
+    
+
+	int led_is_on = LED_SetupMainloop(dev, PIN0, GPIO_OUTPUT_ACTIVE | FLAGS0);
+    TEST_ASSERT_TRUE_MESSAGE(!led_is_on, "LED0 flag should start toggled off");
+
+    led_is_on = LED_ToggleMainloop(dev, PIN0, led_is_on);
+    TEST_ASSERT_TRUE_MESSAGE(led_is_on, "LED0 flag should be toggled on");
+    TEST_ASSERT_TRUE_MESSAGE(gpio_pin_get(dev, PIN0) , "GPIO0 should be toggled on");
 }
 
-void test_multiplication(void)
+void test_thread_enable(void)
 {
-    int x = 5;
-    int y = 6;
-    int z = x * y;
-    TEST_ASSERT_TRUE_MESSAGE(z == 30, "Multiplication of two integers returned incorrect value.");
+	const struct device *dev = device_get_binding(LED1);
+    int counter;
+
+	int led_is_on = LED_SetupThread(dev, PIN1, GPIO_OUTPUT_ACTIVE | FLAGS1, &counter);
+    TEST_ASSERT_TRUE_MESSAGE(!led_is_on, "LED1 flag should start toggled off");
+
+    led_is_on = LED_ToggleThread(dev, PIN1, led_is_on, &counter);
+    TEST_ASSERT_TRUE_MESSAGE(led_is_on, "LED1 flag should be toggled on");
+    TEST_ASSERT_TRUE_MESSAGE(gpio_pin_get(dev, PIN1) , "GPIO1 should be toggled on");
 }
 
 int main (void)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_variable_assignment);
-    RUN_TEST(test_multiplication);
+    RUN_TEST(test_main_loop_enable);
+    RUN_TEST(test_thread_enable);
     return UNITY_END();
 }
